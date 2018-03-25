2aa2dc2e92ba20b4109d08702f09393da4086a1e
Igor Bruno <kazmiller.v@gmail.com>
2018-03-25 14:43:50 -0300

    Desenvolvi a tela de cadastro, e a ligação com a classe respectiva do cadastro (apenas a ligação, a lógica ainda não). Configurei o arquivo AndroidManifest.xml para iniciar a partir do módulo de cadastro.

--- SGF/app/src/main/java/sgf/ssoftware/com/br/sgf/activity/cadastro/CadastroActivity.java
+++ SGF/app/src/main/java/sgf/ssoftware/com/br/sgf/activity/cadastro/CadastroActivity.java
@@ -1,18 +1,18 @@
 //Alterado em 25/03/2018
-package sgf.ssoftware.com.br.sgf.activity.main;
+package sgf.ssoftware.com.br.sgf.activity.cadastro;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import sgf.ssoftware.com.br.sgf.R;
 import sgf.ssoftware.com.br.sgf.teste.cadastro.TesteInserirUsuarioSGF;
 
-public class SGF extends AppCompatActivity {
+public class CadastroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sgf);
+        setContentView(R.layout.activity_cadastro);
     }
 }
