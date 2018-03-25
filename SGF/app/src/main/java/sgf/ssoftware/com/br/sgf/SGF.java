58253ca9529d652d115078f4e3f41eeddff1d00a
Igor Bruno <kazmiller.v@gmail.com>
2018-03-24 23:39:42 -0300

    Projeto base (primeiro commit)

---
+++ SGF/app/src/main/java/sgf/ssoftware/com/br/sgf/SGF.java
@@ -0,0 +1,13 @@
+package sgf.ssoftware.com.br.sgf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SGF extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sgf);
+    }
+}
